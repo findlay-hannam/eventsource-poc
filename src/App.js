53f7ad8da755dd5fc24b3408d75455c31873f2d4
findlay-hannam <f.hannam@elsevier.com>
2021-08-12 09:57:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
+import { EventSource } from 'launchdarkly-eventsource';
+import { SSE } from 'sse.js';
 import logo from './logo.svg';
 import './App.css';
+
+function sendRequest() {
+  const source = new SSE('https://localhost.mendeley.com:3000/query-intent',
+    {
+      payload: JSON.stringify({ a: 'b' }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      withCredentials: false,
+      skipDefaultHeaders: true,
+    },
+  );
+  source.addEventListener('message', console.log);
+  source.addEventListener('error', (err) => {
+    console.error(err);
+    source.close();
+  });
+  source.stream();
+  // const config = {
+  //   method: 'POST',
+  //   'Content-Type': 'application/json',
+  //   body: JSON.stringify({ a: 'b' }),
+  //   skipDefaultHeaders: true,
+  // };
+  // const eventSource = new EventSource('https://localhost.mendeley.com:3000/query-intent', config);
+  // eventSource.addEventListener('message', console.log);
+  // eventSource.onerror = (error) => {
+  //   console.error(error);
+  //   eventSource.close();
+  // };
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={sendRequest}>Send request</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
